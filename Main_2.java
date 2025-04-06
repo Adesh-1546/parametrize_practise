e28a3dda6478ca3fea62dda5c8f997b49d777dee
Adesh-1546 <adeshpol1234@gmail.com>
2025-04-06 11:32:00 +0530

    Add files via upload

---
+++ Main_2.java
@@ -0,0 +1,16 @@
+package com.assignment.constructor.prac;
+//Employee class display details
+
+public class Main_2 {
+	public static void main(String[] args) {
+		Employee em = new Employee(154, "Adesh ", "Java Developer" , 45000); 
+		Employee em1 = new Employee(124, " Sahil", "Web Developer" , 55000); 
+		em.displaydetails();
+		em1.displaydetails();
+		
+		
+		
+		
+   }
+
+}
